68844d467c83cebbafc2d860c7170b1fba549996
req12345 <req12345@mail.ru>
2021-05-11 22:16:14 +0300

    add dealer_hand hidden, user_coice(menu)

--- game_interface.rb
+++ game_interface.rb
@@ -1,12 +1,12 @@
 require_relative 'card'
 require_relative 'deck'
 require_relative 'player'
+# require_relative 'dealer'
 require 'byebug'
 
 class Game
   BET = 10
 
-  attr_reader :card, :scores
   def initialize
     @bank = 0
     @deck = Deck.new
@@ -19,6 +19,9 @@ class Game
     deal_bet
     deal_cards
     players_hand
+    puts "\nDealer cards:"
+    dealer_hand_hidden
+    user_choice
   end
 
   def deal_bet
@@ -36,11 +39,27 @@ class Game
 
   def players_hand
     total_scores = 0
+    puts 'Your cards:'
     @player.hand do |c|
-    puts "#{c.rank} #{c.suit.encode('utf-8')}"
+    puts "#{c.rank}#{c.suit.encode('utf-8')}"
     total_scores += c.scores
     end
     puts "Total scores: #{total_scores}"
+  end
+
+  def dealer_hand_hidden
+    @dealer.cards.size.times {|c| puts '*'}
+  end
+
+  def user_choice
+    puts "\n1. Skip\n2. Add card\n3. Open cards"
+    choice = gets.chomp.to_i
+
+    case when choice
+    when 1 then skip
+    when 2 then @player.get_card(@deck.draw_card)
+    when 3 then open_cards
+    end
   end
 end
 
