4bee8f1b2ed610bb068895907d2239685fd3dcc4
req12345 <req12345@mail.ru>
2021-05-12 20:55:53 +0300

    hand_scores

--- player.rb
+++ player.rb
@@ -1,5 +1,6 @@
 class Player
   attr_accessor :cards
+  attr_reader :total_scores
 
   def initialize(name)
     @name = name
@@ -9,6 +10,7 @@ class Player
 
   def get_card(card)
    @cards.size < 3 ? @cards << card : 'Already 3 cards'
+   hand_scores
   end
 
   def bet
@@ -19,4 +21,9 @@ class Player
     @cards.each(&block)
   end
 
+  def hand_scores
+    total_scores = 0
+    @cards.each { |card| total_scores += card.scores }
+    total_scores
+  end
 end
