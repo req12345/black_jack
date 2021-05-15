9644a9674f3b802f5d3e036415a8da37dd7b4b43
req12345 <req12345@mail.ru>
2021-05-15 15:36:53 +0300

    refactoring

--- player.rb
+++ player.rb
@@ -1,6 +1,6 @@
 class Player
   attr_accessor :cards, :bank
-  attr_reader :total_scores
+  attr_reader :total_scores, :name
 
   def initialize(name)
     @name = name
@@ -9,8 +9,7 @@ class Player
   end
 
   def get_card(card)
-   @cards.size < 3 ? @cards << card : 'Already 3 cards'
-   hand_scores
+    @cards.size < 3 ? @cards << card : puts('Already 3 cards')
   end
 
   def bet
@@ -23,7 +22,18 @@ class Player
 
   def hand_scores
     total_scores = 0
-    @cards.each { |card| total_scores += card.scores }
+    without_aces = @cards.reject { |card| card.rank == 'A' }
+    if @cards.size == without_aces.size
+      total_scores = @cards.map(&:scores).sum.to_i
+    else
+      total_scores = without_aces.map(&:scores).sum.to_i
+      aces = @cards - without_aces
+
+      aces.each do |ace|
+        scores = total_scores + ace.scores <= 21 ? ace.scores : 1
+        total_scores += scores
+      end
+    end
     total_scores
   end
 end
