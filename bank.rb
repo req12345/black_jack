4dbb40f2e23c1305c241eeb9d0f4bcde5e2a1118
req12345 <req12345@mail.ru>
2021-05-17 07:21:15 +0300

    [add class]

---
+++ bank.rb
@@ -0,0 +1,31 @@
+class Bank
+  BET = 10
+
+  attr_reader :bank
+
+  def initialize
+    @bank = 0
+  end
+
+  def auto_bet(player, dealer)
+    player.bet(BET)
+    dealer.bet(BET)
+    @bank = BET * 2
+  end
+
+  def winner(player)
+    player.win(@bank)
+    @bank = 0
+  end
+
+  def draw(player, dealer)
+    player.win(@bank / 2)
+    player.win(@bank / 2)
+    @bank = 0
+  end
+
+  def no_money?(player, dealer)
+    player.cash > BET
+    dealer.cash > BET
+  end
+end
