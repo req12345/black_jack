8d6e2a744bcd6d8a7a1e553ee693895ac920f7da
req12345 <req12345@mail.ru>
2021-05-17 07:21:46 +0300

    [fix]

--- player.rb
+++ player.rb
@@ -1,10 +1,18 @@
 class Player
-  attr_accessor :cards, :bank, :hand
+  attr_accessor :cards, :bank, :hand, :cash
   attr_reader :total_scores, :name
 
   def initialize(name)
     @name = name
-    @bank = 100
+    @cash = 100
     @hand = Hand.new
+  end
+
+  def bet(money)
+    @cash -= 10
+  end
+
+  def win(bank)
+    @cash += bank
   end
 end
